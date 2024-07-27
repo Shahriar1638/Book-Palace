e6ac56c5f60ae2303d6e5903e9635ed0fcf4c614
Shahriar1638 <shafim1638@gmail.com>
2024-07-27 11:51:18 +0600

    log in and register page adjusted

--- src/Shared/Login And registration/Login.jsx
+++ src/Shared/Login And registration/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
     return (
         <div className="grid grid-cols-7">
@@ -5,12 +7,15 @@ const Login = () => {
             </div>
             <div className="col-span-2 h-screen">
                 <div className="flex flex-col items-center justify-center h-full">
-                    <h1 className="text-4xl font-bold">Login</h1>
+                    <h1 className="text-4xl font-bold">Login to your account</h1>
                     <form className="flex flex-col mt-10 w-[26rem]">
-                        <input type="text" placeholder="Your email" className="border border-solid border-gray-400 rounded-md px-6 py-2" />
-                        <input type="password" placeholder="Your password" className="border border-solid border-gray-400 mt-4 rounded-md px-6 py-2" />
+                        <input type="text" name="email" placeholder="Your email" className="border-b-[1px] border-solid shadow-lg border-gray-400 rounded-md px-6 py-3" />
+                        <input type="password" name="password" placeholder="Your password" className="border-b-[1px] border-solid shadow-lg border-gray-400 mt-4 rounded-md px-6 py-3" />
                         <button className="bg-blue-500 text-white p-2 rounded-md mt-4">Login</button>
                     </form>
+                    <div>
+                        <h1>Dont have an account ? <span className="text-blue-500 underline"> <Link to={"/signup"}> Sign up now !</Link></span></h1>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Shared/Login And registration/Signup.jsx
+++ src/Shared/Login And registration/Signup.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Signup = () => {
     return (
-        <div>
-            
+        <div className="grid grid-cols-7">
+            <div className="col-span-5 h-screen pl-44">
+                <div className="flex flex-col items-start justify-center h-full">
+                    <h1 className="text-4xl font-bold">Create to your account</h1>
+                    <form className="flex flex-col mt-10 w-[46rem]">
+                        <input type="text" placeholder="Your email" name="email" className="border-b-[1px] border-solid shadow-lg border-gray-400 rounded-md px-6 py-3" />
+                        <input type="password" placeholder="Your password" name="password" className="border-b-[1px] border-solid shadow-lg border-gray-400 mt-4 rounded-md px-6 py-3" />
+                        <button className="bg-blue-500 text-white p-2 rounded-md mt-4">Sign up</button>
+                    </form>
+                    <div>
+                        <h1>Already have an account ? <span className="text-blue-500 underline"> <Link to={"/login"}> go to log in</Link></span></h1>
+                    </div>
+                </div>
+            </div>
+            <div className="col-span-2 h-screen bg-purple-500">
+            </div>
         </div>
     );
 };
